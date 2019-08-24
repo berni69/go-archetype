29accac4cbe6b888b5bb7e07af4e630687599a43
berni69 <berni.emerald@gmail.com>
2019-08-24 23:24:30 +0200

    Added config as Global variable and auto-refresh every minut

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/berni69/go-archetype/consul"
 	"github.com/berni69/go-archetype/utils"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,39 +15,45 @@ func helloWorld(resp http.ResponseWriter, req *http.Request) {
 
 	log.Debug("Hello world")
 	resp.Write([]byte("Hello world"))
-	var generic []interface{}
-	var err = utils.GetJson("https://api.github.com/users/hadley/orgs", &generic)
-	if err != nil {
-		log.Debug(err)
-		return
-	}
-	j, err := json.Marshal(generic)
-	if err != nil {
-		log.Debug(err)
-		return
-	}
-	resp.Write(j)
+}
 
+func discoverAirport(resp http.ResponseWriter, req *http.Request) {
+	url, err := consul.LookupService("airport")
+	if err != nil {
+		resp.Write([]byte(err.Error()))
+	}
+
+	log.Debug(url)
+	resp.Write([]byte(url))
+}
+
+func getConfig() {
+
+	err := consul.LoadConsulConfig(utils.GetEnv("CONSUL_KV", ""), &Configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	log.Info("Starting Server")
-	var config configuration
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	// Logging options
 	utils.InitLogger()
+	/** Loading Config **/
+	c := cron.New()
+	getConfig()
+	c.AddFunc("0 * * * * *", func() { getConfig() }) //Retrieve the config every minut
+	c.Start()
 
-	addressPort := utils.GetEnv("ADDRESS_PORT", ":8000")
+	/* Starting new Router */
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", helloWorld).Methods("GET")
-
-	log.Debug(consul.LookupService("airport"))
-	err = consul.LoadConsulConfig(utils.GetEnv("CONSUL_KV", ""), &config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	router.HandleFunc("/discover", discoverAirport).Methods("GET")
+	addressPort := utils.GetEnv("ADDRESS_PORT", ":8000")
 	log.Fatal(http.ListenAndServe(addressPort, router))
+
 }
